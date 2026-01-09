85e08332a9733ef2b53c2ff35621a77a80d380c4
Ulathi Ncube <lathincube@gmail.com>
2026-01-09 14:38:22 +0200

    part1-step1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Header({ course }) {
+  return <h1>{course}</h1>;
+}
+
+function Content({ part, exercises }) {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+}
+
+function Total({ exercises }) {
+  const totalExercises = exercises.reduce(
+    (accumulator, current) => accumulator + current,
+    0
+  );
+  return <p>Number of exercises {totalExercises}</p>;
+}
+
+function App() {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+}
+
+export default App;
