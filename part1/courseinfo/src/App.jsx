3edacd7fe53428164d67b4242f08c31a01b2890c
Ulathi Ncube <lathincube@gmail.com>
2026-01-09 15:35:10 +0200

    part1-step3&4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -24,33 +24,38 @@ function Content({ parts }) {
 
 function Total({ exercises }) {
   const totalExercises = exercises.reduce(
-    (accumulator, current) => accumulator + current,
+    (accumulator, current) => accumulator + current.exercises,
     0
   );
   return <p>Number of exercises {totalExercises}</p>;
 }
 
 function App() {
-  const course = 'Half Stack application development';
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const { name, parts } = course;
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total exercises={parts} />
     </div>
   );
 }
